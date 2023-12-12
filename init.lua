5a93965dc48e8d81fa2eef4bb44564ef90e76dc1
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-11 21:47:25 -0300

    understanding neovim - part 2.1 - finish editor setup

--- init.lua
+++ init.lua
@@ -13,9 +13,22 @@ vim.opt.wrap = false
 -- tabs
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
+vim.opt.shiftwidth = 0 -- fallback to tabstop
 
 -- clipboard
 vim.opt.clipboard = "unnamedplus"
 
 -- padding
-vim.opt.scrolloff=999
+vim.opt.scrolloff = 999
+
+--- visual editor
+vim.opt.virtualedit = "block"
+
+--- inccommand
+vim.opt.inccommand = "split"
+
+--- ignorecase for searching commands
+vim.opt.ignorecase = true
+
+--- terminal colors 
+vim.opt.termguicolors = true
