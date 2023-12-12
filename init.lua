29f8d0ccc5814946201795054e199773ac042c6f
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-11 21:22:47 -0300

    understanding neovim - part 1

---
+++ init.lua
@@ -0,0 +1,21 @@
+print("Hello from neovim")
+
+-- setup editor
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- text wrap
+vim.opt.wrap = false
+
+-- tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+
+-- clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- padding
+vim.opt.scrolloff=999
