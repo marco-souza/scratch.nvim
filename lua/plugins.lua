531deca5c34ff720f5b6dbb20af3f7a24969af0b
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-17 18:42:01 -0300

    fea: add telescope keys

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,6 +35,12 @@ local plugins = {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>ff", function(opts) require('telescope.builtin').find_files(opts) end, desc = "Find files" },
+      { "<leader>fw", function(opts) require('telescope.builtin').live_grep(opts) end, desc = "Find Word" },
+      { "<leader>fb", function(opts) require('telescope.builtin').buffers(opts) end, desc = "Find in buffer" },
+      { "<leader>fh", function(opts) require('telescope.builtin').help_tags(opts) end, desc = "Find help" },
+    },
   },
   {
     "marco-souza/ncm.nvim",
