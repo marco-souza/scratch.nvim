ebb4a8c2128291315e01d1fb0b514390403ac956
Marco Antônio <ma.souza.junior@gmail.com>
2023-12-20 15:41:12 -0300

    fea: add neorg

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -132,6 +132,26 @@ local plugins = {
       vim.o.timeoutlen = 300
     end,
   },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/workspace/marco-souza/notes/",
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
   { "wakatime/vim-wakatime" },
 }
 
