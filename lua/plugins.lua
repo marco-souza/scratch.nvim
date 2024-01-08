a37c92df5b329205abb476432c4796bd8eddc7c4
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-08 20:34:27 -0300

    fea: adding incremental selection

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,6 +15,15 @@ local configs = {
       auto_install = true,
       indent = { enable = true },
       highlight = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<leader>ss",
+          node_incremental = "<leader>si",
+          scope_incremental = "<leader>sc",
+          node_decremental= "<leader>sd",
+        },
+      },
     })
   end,
   theme = function()
