d14d2bc65b6d439747fd2009ca6ab0b0bdb40d80
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-09 16:13:50 -0300

    enh: setup format on save

--- lua/plugins/formatters.lua
+++ lua/plugins/formatters.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       local null_ls = require("null-ls")
       local b = null_ls.builtins
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
       null_ls.setup({
         sources = {
@@ -28,14 +29,24 @@ return {
           b.diagnostics.trail_space,
           b.code_actions.refactoring,
         },
+        on_attach = function(client, bufnr)
+          -- format on save
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
       })
 
-      vim.keymap.set(
-        "n",
-        "<leader>gf",
-        vim.lsp.buf.format,
-        { desc = "Format buffer" }
-      )
+      vim.keymap.set("n", "gf", vim.lsp.buf.format, { desc = "Format buffer" })
     end,
   },
 }
