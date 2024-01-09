1ca10abfa500dc3d5c5d18fc32a9fd442e1ffdbc
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-09 13:04:43 -0300

    fea: add telescope ui select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -33,4 +33,17 @@ return {
       },
     },
   },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {}
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
