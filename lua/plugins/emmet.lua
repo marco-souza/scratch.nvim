4200b89fa6b43a154a8db6949b28ae9f44c79176
Marco Antônio <ma.souza.junior@gmail.com>
2024-01-09 16:01:19 -0300

    fea: setup emmet ls

---
+++ lua/plugins/emmet.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "aca/emmet-ls",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        init_options = {
+          html = {
+            options = {
+              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+              ["bem.enabled"] = true,
+            },
+          },
+        },
+      })
+    end,
+  },
+}
