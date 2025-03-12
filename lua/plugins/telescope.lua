3d3f38f55f5394ed9887a6c76dfa6654e896c0b3
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-12 16:14:06 -0300

    enh(telescope): ignore some files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -38,6 +38,13 @@ return {
     },
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = {
+            ".git",
+            "node_modules",
+          },
+        },
+
         pickers = {
           find_files = {
             hidden = true,
