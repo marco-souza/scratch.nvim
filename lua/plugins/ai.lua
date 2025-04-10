94298d831f2c30a3f977915c6469ed01c638a28f
Marco Antônio <ma.souza.junior@gmail.com>
2025-04-10 14:12:10 -0300

    fea(ai): add mcphub.nvim plugin

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -66,4 +66,18 @@ return {
       },
     },
   },
+
+  -- mcp.nvim (extend llm funcitonality with tools)
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for Job and HTTP requests
+    },
+    -- comment the following line to ensure hub will be ready at the earliest
+    cmd = "MCPHub", -- lazy load by default
+    build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
+    -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
+    -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
+    config = true,
+  },
 }
