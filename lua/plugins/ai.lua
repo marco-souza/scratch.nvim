40533737c46cee8128c4a8b61108b2b1190e38db
Marco Antônio <ma.souza.junior@gmail.com>
2025-05-03 09:12:11 -0300

    enh: update mcp config

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -91,8 +91,7 @@ return {
     build = "npm install -g mcp-hub@latest", -- Installs required mcp-hub npm module
     -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
     -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
-    opts = {
-      auto_approve = true,
-    },
+    ---@type MCPHubOpts
+    opts = {},
   },
 }
