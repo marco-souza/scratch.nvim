0bef6ba194fca1ec6d5ec43429b0f21428bc3e0a
Marco Antônio <ma.souza.junior@gmail.com>
2025-05-08 15:54:48 -0300

    fea: add rest client

--- lua/plugins/devtools.lua
+++ lua/plugins/devtools.lua
@@ -1,18 +1,14 @@
-vim.g.vrc_set_default_mapping = 0
-vim.g.vrc_response_default_content_type = "application/json"
-vim.g.vrc_output_buffer_name = "application/json"
-vim.g.vrc_auto_format_response_patterns = {
-  json = "jq",
-}
-
 return {
   {
-    "diepm/vim-rest-console",
+    "marco-souza/rest.nvim",
+    dir = "~/w/marco-souza/rest.nvim/",
+    cmd = "Rest",
+    opts = {},
     keys = {
       {
-        "<leader>xr",
-        ":call VrcQuery()<CR>",
-        desc = "Open Request Console",
+        "<leader>r",
+        ":Rest open<CR>",
+        desc = "Open Rest Client",
       },
     },
   },
