816d392d7cc040144d4266f2bc8aeafabb27e13e
Marco Antônio <ma.souza.junior@gmail.com>
2025-07-03 18:25:36 -0300

    fea: add ollero back

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,71 +1,81 @@
 return {
+  {
+    "marco-souza/ollero.nvim",
+    event = "VeryLazy",
+    dir = "~/w/marco-souza/ollero.nvim",
+    opts = {},
+    -- build = ":!go install github.com/marco-souza/omg@latest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "marco-souza/term.nvim",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+  },
 
   -- avante.nvim (nvim cursor-like)
   {
-    {
-      "yetone/avante.nvim",
-      event = "VeryLazy",
-      version = false, -- set this if you want to always pull the latest change
-      opts = {
-        provider = "copilot",
-        cursor_applying_provider = "groq",
-        hints = { enable = true },
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "copilot",
+      cursor_applying_provider = "groq",
+      hints = { enable = true },
 
-        behaviour = {
-          enable_cursor_planning_mode = true, -- enable cursor planning mode!
-        },
-
-        providers = {
-          groq = { -- define groq provider
-            __inherited_from = "openai",
-            api_key_name = "GROQ_API_KEY",
-            endpoint = "https://api.groq.com/openai/v1/",
-            model = "llama-3.3-70b-versatile",
-          },
-        },
-
-        -- MCPHub integration
-        system_prompt = function()
-          local hub = require("mcphub").get_hub_instance()
-          if hub then
-            return hub:get_active_servers_prompt()
-          end
-        end,
-        custom_tools = function()
-          return {
-            require("mcphub.extensions.avante").mcp_tool(),
-          }
-        end,
+      behaviour = {
+        enable_cursor_planning_mode = true, -- enable cursor planning mode!
       },
-      build = "make",
-      dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        --- The below dependencies are optional,
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        {
-          "zbirenbaum/copilot.lua", -- for providers='copilot'
-          cmd = "Copilot",
-          event = "LspAttach",
-          opts = {
-            suggestion = { enabled = true },
-            panel = { enabled = true },
-            filetypes = {
-              markdown = true,
-              help = true,
-            },
+
+      providers = {
+        groq = { -- define groq provider
+          __inherited_from = "openai",
+          api_key_name = "GROQ_API_KEY",
+          endpoint = "https://api.groq.com/openai/v1/",
+          model = "llama-3.3-70b-versatile",
+        },
+      },
+
+      -- MCPHub integration
+      system_prompt = function()
+        local hub = require("mcphub").get_hub_instance()
+        if hub then
+          return hub:get_active_servers_prompt()
+        end
+      end,
+      custom_tools = function()
+        return {
+          require("mcphub.extensions.avante").mcp_tool(),
+        }
+      end,
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
+        cmd = "Copilot",
+        event = "LspAttach",
+        opts = {
+          suggestion = { enabled = true },
+          panel = { enabled = true },
+          filetypes = {
+            markdown = true,
+            help = true,
           },
         },
-        {
-          -- Make sure to set this up properly if you have lazy=true
-          "MeanderingProgrammer/render-markdown.nvim",
-          opts = {
-            file_types = { "markdown", "Avante" },
-          },
-          ft = { "markdown", "Avante" },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
         },
+        ft = { "markdown", "Avante" },
       },
     },
   },
