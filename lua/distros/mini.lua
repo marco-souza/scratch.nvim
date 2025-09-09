0638a5d637225023c3f913cc4f8c9945b8d13f34
Marco Antônio <ma.souza.junior@gmail.com>
2025-09-09 19:06:21 -0300

    enh: update minimal distro

--- lua/distros/mini.lua
+++ lua/distros/mini.lua
@@ -15,15 +15,18 @@ vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevelstart = 0
 
+-- term colors
+vim.opt.termguicolors = true
+
 vim.keymap.set("n", "<leader>w", ":write<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
 
 -- plugins
 vim.pack.add({
-  { src = "https://github.com/vague2k/vague.nvim" },
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/echasnovski/mini.pick" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   { src = "https://github.com/github/copilot.vim" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/NeogitOrg/neogit" },
@@ -34,7 +37,6 @@ vim.pack.add({
   { src = "https://github.com/ibhagwan/fzf-lua" },
 })
 
-vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
 -- plugin setup
@@ -48,16 +50,20 @@ vim.keymap.set("n", "<leader>fh", ":Pick help<CR>", { desc = "Find help" })
 
 vim.keymap.set("n", "<leader>e", ":Oil<CR>")
 
-vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", { desc = "Neogit toggle" })
+vim.keymap.set("n", "<leader>G", ":Neogit<CR>", { desc = "Neogit toggle" })
 
--- lsp
-vim.lsp.enable({
-  "lua_ls",
-  "gopls",
-  "markdown",
-  "expert_ls",
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go to [d]efinition", buffer = true })
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { desc = "[r]ename", buffer = true })
+vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, { desc = "code [a]ction", buffer = true })
+
+-- treesitter
+vim.treesitter.language.register('go', {
+  'gomod',
+  'gowork',
+  'gotmpl',
 })
 
+-- lsp
 vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
@@ -76,9 +82,15 @@ vim.lsp.config("lua_ls", {
   },
 })
 
+vim.lsp.enable({
+  "lua_ls",
+  "gopls",
+})
+
 -- completions
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
+    print("LSP attached", ev)
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     if client == nil then
       return
